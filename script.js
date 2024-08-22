d6107408eca4185d76caed290a79d736c3c1f8b5
menu95 <100180464+menu95@users.noreply.github.com>
2024-08-22 13:08:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,176 +1,149 @@
-// script.js
-import { playGoogleDriveVideo } from "./googleDrive.js";
-import { playYouTubeVideo } from "./youtube.js";
-// Evento que é disparado quando o conteúdo da página é carregado
-document.addEventListener("DOMContentLoaded", () => {
-  // Seleciona os elementos do DOM que serão usados no script
-  const playerContainer = document.getElementById("player-container");
-  const videoPlayer = document.getElementById("video-player");
+// youtube.js
+export const youtubeChannels = [
+  {
+    channelName: "YouTube Playlist - picapau",
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/or5h9OugzLI?si=AoMf6iHAxJeZjm0P&amp;controls=0",
+        duration: 7 * 60 * 60 + 49 * 60 + 2, // 7 hours 49 min 02 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/C-Vy5T-bGY8?si=6auUxeff16Mso8sF&amp;controls=0",
+        duration: 2 * 60 * 60 + 34 * 60 + 2, // 2 hours 34 min 02 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/LVE17tSApHI?si=X4tBcTc3hBFy0KQl&amp;controls=0",
+        duration: 2 * 60 * 60 + 33 * 60 + 31, // 2 hours 33 min 31 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/mtCIHV5wuEw?si=Qb_WF5FHmr6kYH6x&amp;controls=0",
+        duration: 2 * 60 * 60 + 33 * 60 + 47, // 2 hours 33 min 47 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/k1b9ND7-jjI?si=Bsn0SXuKLyaM4URW&amp;controls=0",
+        duration: 2 * 60 * 60 + 33 * 60 + 31, // 2 hours 33 min 31 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/Wghfy0HGVx0?si=Ew62qUkn0-MIuEGy&amp;controls=0",
+        duration: 2 * 60 * 60 + 33 * 60 + 31, // 2 hours 33 min 31 sec
+      },
+    ],
+  },
 
-  // Configura o player de vídeo usando a biblioteca Video.js
-  const videoJsPlayer = videojs(videoPlayer, {
-    autoplay: true,
-    controls: true,
-    controlBar: {
-      fadeIn: true,
-      fadeOut: true,
-    },
-    fluid: true, // Garante que o player de vídeo seja responsivo
-  });
+  {
+    channelName: "YouTube Playlist - Tom e Jerry",
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/aGUaNqitFPA?si=Tb5URe909bOnEKx7&amp;controls=0",
+        duration: 59 * 60 + 29, // 59 min 29 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/px0whOy_BCE?si=Cq91fQWR5jyRhSn1&amp;controls=0",
+        duration: 59 * 60 + 5, // 59 min 05 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/AnrlzQyYapY?si=sA-m-d1nkwi-Qkgo&amp;controls=0",
+        duration: 59 * 60 + 50, // 59 min 50 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/e0zxuBzGYnI?si=sUJGaWjj7lWJZAw8&amp;controls=0",
+        duration: 1 * 60 * 60 + 1 * 60 + 23, // 1 hours 01 min 23 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/rLbY-xZQUyw?si=sHRo9yUH6-ZA2660&amp;controls=0",
+        duration: 58 * 60 + 33, // 58 min 33 sec
+      },
+      {
+        src: "https://www.youtube.com/embed/zpSHhkvLQMA?si=QZEzejxpyd64xFk9&amp;controls=0",
+        duration: 59 * 60 + 51, // 59 min 51 sec
+      },
+    ],
+  },
 
-  let currentPlayer = null;
-  let isGoogleDriveVideoSelected = false;
+  {
+    channelName: "YouTube Playlist - Todo mundo odeia o chris",
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/0ssLZUuPQpM?si=9ave2_2IMLITtXop&amp;controls=0",
+        duration: 33,
+      },
+    ],
+  },
+  {
+    channelName: "YouTube Playlist - Eu a patroa e as crianças",
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/KT0u74pyo3Q?si=7gYLwPk_uY8c9zOe&amp;controls=0",
+        duration: 3 * 60 * 60 + 57 * 60 + 40, // 1 temporada 3 horas 57 min 40 seg
+      },
+      {
+        src: "https://www.youtube.com/embed/ZIsq7WWTu7s?si=4bQAMzwAGLw-jH-V&amp;controls=0",
+        duration: 10 * 60 * 60 + 12 * 60 + 57, // 2 temporada 10 horas 12 min 57 seg
+      },
+      {
+        src: "https://www.youtube.com/embed/c-XVaNOouQk?si=6BFA-iYMFk4FWgYI&amp;controls=0",
+        duration: 9 * 60 * 60 + 38 * 60 + 28, // 3 temporada 9 horas 38 min 28 seg
+      },
+      {
+        src: "https://www.youtube.com/embed/mrtD9U3zTso?si=a1xLG_f3DpDt4nce&amp;controls=0",
+        duration: 10 * 60 * 60 + 40 * 60 + 5, // 4 temporada 10 horas 40 min 05 seg
+      },
+      {
+        src: "https://www.youtube.com/embed/rDohXLxqcdw?si=2ETG_OWHf-R7gNdg&amp;controls=0",
+        duration: 9 * 60 * 60 + 26 * 60 + 52, // 5 temporada 9 horas 26 min 52 seg
+      },
+    ],
+  },
+  {
+    channelName: "YouTube Playlist - O show dos looney tunes",
+    videos: [
+      {
+        src: "https://www.youtube.com/embed/9hvc_XDLs1o?si=3WO_bZbkq6otZ1at&amp;controls=0",
+        duration: 1 * 60 * 60 + 10 * 60 + 55, // 1 horas 10 min 55 seg
+      },
+      {
+        src: "https://www.youtube.com/embed/6tZMpieIDgc?si=ISMCrZpXSS04PJbo&amp;controls=0",
+        duration: 2 * 60 * 60 + 3 * 60 + 29, // 2 horas 03 min 29 seg
+      },
+    ],
+  },
+];
 
-  // Lista de canais e vídeos
-  const channelList = document.getElementById("channel-list");
-  const channelItems = channelList.querySelectorAll("li");
-  let currentIndex = 0;
-  let closeListTimeout;
+const startTime = new Date("2024-05-28T11:10:00Z");
 
-  // Atualiza a seleção visual dos canais
-  const updateVisualSelection = () => {
-    channelItems.forEach((item, index) => {
-      if (index === currentIndex) {
-        item.classList.add("selected");
-        item.scrollIntoView({ behavior: "smooth", block: "nearest" });
-      } else {
-        item.classList.remove("selected");
-      }
-    });
-  };
+export const getCurrentVideoYouTube = (channelIndex) => {
+  const videos = youtubeChannels[channelIndex].videos;
+  const now = new Date();
+  const elapsedTime = Math.floor((now - startTime) / 1000);
+  const totalDuration = videos.reduce((acc, video) => acc + video.duration, 0);
+  const loopTime = elapsedTime % totalDuration;
+  let currentTime = 0;
 
-  // Para o player atual
-  const stopCurrentPlayer = () => {
-    if (currentPlayer) {
-      if (currentPlayer.tagName.toLowerCase() === "video") {
-        const videoJsPlayer = videojs(currentPlayer);
-        videoJsPlayer.dispose();
-      } else if (currentPlayer.tagName.toLowerCase() === "iframe") {
-        currentPlayer.remove();
-      }
+  for (let video of videos) {
+    currentTime += video.duration;
+    if (loopTime < currentTime) {
+      const startOffset = loopTime - (currentTime - video.duration);
+      return {
+        src: video.src,
+        startOffset,
+        duration: video.duration - startOffset,
+      };
     }
-  };
+  }
+};
 
-  // Inicia o tempo limite para fechar a lista de canais
-  const startCloseListTimeout = () => {
-    clearTimeout(closeListTimeout);
-    closeListTimeout = setTimeout(() => {
-      channelList.style.left = "-300px";
-    }, 4000);
-  };
+export const playYouTubeVideo = (playerContainer, channelIndex) => {
+  let currentVideo = getCurrentVideoYouTube(channelIndex);
+  playerContainer.innerHTML = `
+    <iframe 
+      id="current-video"
+      src="${currentVideo.src}&start=${currentVideo.startOffset}" 
+      width="100%" 
+      height="100%" 
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe>`;
 
-  // Atualiza a fonte do vídeo com base na seleção do canal
-  const updateVideoSource = () => {
-    const selectedItem = channelItems[currentIndex];
-    const newSrc = selectedItem.getAttribute("data-src");
-    const newType = selectedItem.getAttribute("data-type");
-
-    stopCurrentPlayer();
-
-    if (newType === "m3u8") {
-      //aqui entra a logica para carregar o canal do m3u8 na tela
-      playerContainer.innerHTML =
-        '<video id="video-player" class="video-js vjs-default-skin" controls autoplay></video>';
-      currentPlayer = document.getElementById("video-player");
-      const videoJsPlayer = videojs(currentPlayer, {
-        autoplay: true,
-        controls: true,
-        controlBar: {
-          fadeIn: true,
-          fadeOut: true,
-        },
-        fluid: true,
-      });
-      videoJsPlayer.src({ type: "application/x-mpegURL", src: newSrc });
-      videoJsPlayer.play();
-      isGoogleDriveVideoSelected = false;
-    } else if (newType === "iframe") {
-      if (selectedItem.getAttribute("data-channel") === "google-drive-video") {
-        //aqui entra a logica para carregar o canal do googledrive na tela
-        const channelIndex = parseInt(
-          selectedItem.getAttribute("data-channel-index"),
-          10
-        );
-        console.log(playerContainer); //é a div com a url e os demais informações
-        console.log("entrei no google drive");
-        playGoogleDriveVideo(playerContainer, channelIndex);
-        isGoogleDriveVideoSelected = true;
-      } else if (
-        selectedItem.getAttribute("data-channel") === "youtube-video"
-      ) {
-        //aqui entra a logica para carregar o canal do youtube na tela
-        console.log("entrei no youtube");
-
-        const channelIndex = parseInt(
-          selectedItem.getAttribute("data-channel-index"),
-          10
-        );
-        playYouTubeVideo(playerContainer, channelIndex);
-        isGoogleDriveVideoSelected = false;
-
-        //channel index é o numero do canal na lista 0,1,2
-      } else {
-        // Handle other iframe videos if needed
-        playerContainer.innerHTML = `<iframe src="${newSrc}" width="100%" height="100%" allow="autoplay" style="border:none;" tabindex="0"></iframe>`;
-        currentPlayer = playerContainer.querySelector("iframe");
-        currentPlayer.focus();
-        isGoogleDriveVideoSelected = false;
-        console.log("seleção de canal falhou");
-      }
-    }
-  };
-
-  // Atualiza a seleção visual ao carregar
-  updateVisualSelection();
-
-  // Função para abrir o menu de canais
-  const openChannelMenu = () => {
-    if (channelList.style.left === "0px") {
-      updateVideoSource();
-      channelList.style.left = "-300px";
-    } else if (
-      isGoogleDriveVideoSelected &&
-      document.activeElement === currentPlayer
-    ) {
-      currentPlayer.focus(); // Garante que o foco está no iframe para reproduzir
-      document.activeElement.blur(); // Remove o foco para permitir que o iframe receba entrada
-      currentPlayer.contentWindow.document.querySelector("video").play();
-      isGoogleDriveVideoSelected = false;
-    } else {
-      channelList.style.left = "0";
-      startCloseListTimeout();
-    }
-  };
-
-  // Eventos de teclado para navegar e selecionar canais
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" || e.key === "Backspace") {
-      openChannelMenu();
-    } else if (e.key === "ArrowDown") {
-      if (channelList.style.left === "0px") {
-        currentIndex = (currentIndex + 1) % channelItems.length;
-        updateVisualSelection();
-        startCloseListTimeout();
-      }
-    } else if (e.key === "ArrowUp") {
-      if (channelList.style.left === "0px") {
-        currentIndex =
-          (currentIndex - 1 + channelItems.length) % channelItems.length;
-        updateVisualSelection();
-        startCloseListTimeout();
-      }
-    }
-  });
-
-  // Evento de clique para selecionar canais
-  channelItems.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      currentIndex = index;
-      updateVisualSelection();
-      updateVideoSource();
-    });
-  });
-
-  // Evento de movimento do mouse para reiniciar o tempo limite para fechar a lista de canais
-  channelList.addEventListener("mousemove", startCloseListTimeout);
-});
+  let currentPlayer = document.getElementById("current-video");
+  currentPlayer.focus();
+};
